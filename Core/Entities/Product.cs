7ce0cb92a4ffb7b6345abdba03f2fcee4f9f35df
dpeichev89 <dpeichev89@gmail.com>
2024-02-10 07:41:55 +0000

    office update 1

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -6,7 +6,7 @@ namespace Core.Entities
     {
         public string Name { get; set; }
 
-        public string Descriptio { get; set; }
+        public string Description { get; set; }
 
         public decimal Price { get; set; }  
 
